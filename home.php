1b97765a236915679bc9fb11c9d14ac212ffbe25
ouissal23 <149329248+ouissal23@users.noreply.github.com>
2024-11-12 19:46:48 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="navbar.css">
+    <title>Document</title>
+   
+</head>
+<body>
+    <?php
+    include("navbar.php");
+    echo 'Welcome to Home , your information:<br>';
+    echo 'Email:' . $_SESSION["emailS"]. '<br> ';
+    echo 'Password:' . $_SESSION["passS"].'<br>' ;
+    ?>
+
+</body>
+</html> 
