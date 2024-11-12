6354d4e70d4a4df6f01892618b41c52be8d4dfc9
ouissal23 <149329248+ouissal23@users.noreply.github.com>
2024-11-12 19:37:10 +0100

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Sign Up</title>
+</head>
+<body>
+    <div class="signup-box">
+    <h1>Sign Up </h1>
+    <form>
+        <label>First Name</label>
+        <input type="text" placeholder=""> <br>
+        <label>Last Name</label>
+        <input type="text" placeholder=""><br>
+        <label>Email</label>
+        <input type="email" placeholder=""><br>
+        <label>Password</label>
+        <input type="password" placeholder=""><br>
+        <label>Confirm Password</label>
+        <input type="password" placeholder=""><br>
+        <button type="submit" class="btn">Sign Up</button>
+
+    
+    
+
+    </form>
+</div>
+</body>
+</html>
