869b823509106ad193d2f8d4d321e919d58a476f
ouissal23 <149329248+ouissal23@users.noreply.github.com>
2024-11-12 19:48:24 +0100

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+    <div class="navdiv">
+      <div class="logo"><a href="#">Sweeties</a></div>
+      <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="page.php">Page</a></li>
+        <li><a href="#">Username</a></li>
+
+        <button><a href="login.php">Logout</a></button>
+      </ul>
+    </div>
+  </nav>
